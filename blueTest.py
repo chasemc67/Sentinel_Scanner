de1e0a5f629203c4ab5f24a687c7149523ab56ce
Chase <chasemc67@gmail.com>
2017-02-06 23:34:11 +0000

    bluetooth stuff

---
+++ blueTest.py
@@ -0,0 +1,15 @@
+import bluetooth
+
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("found %d devices" % len(nearby_devices))
+
+for addr, name in nearby_devices:
+	print(" %s - %s"  % (addr, name))
+	print(str(bluetooth.hci_get_route(addr)))
+
+#for A in dir(nearby_devices):
+#	print(str(A)+ "\n")
+
+#for f in nearby_devices:
+#	print(str(f))
+
