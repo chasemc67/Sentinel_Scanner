c70b30aae6875db8f578395a6bc35fcc7d5ef85f
chasemc67 <chasemc67@gmail.com>
2017-02-08 15:23:22 -0700

    Stuff

--- blueTest.py
+++ blueTest.py
@@ -5,7 +5,7 @@ print("found %d devices" % len(nearby_devices))
 
 for addr, name in nearby_devices:
 	print(" %s - %s"  % (addr, name))
-	print(str(bluetooth.hci_get_route(addr)))
+	#print(str(bluetooth.hci_get_route(addr)))
 
 #for A in dir(nearby_devices):
 #	print(str(A)+ "\n")
