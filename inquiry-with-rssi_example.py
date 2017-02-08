06399235816216b03a381d5f148f2ce8f426c95f
chasemc67 <chasemc67@gmail.com>
2017-02-08 15:58:51 -0700

    trying RSSI for specific device

--- inquiry-with-rssi_example.py
+++ inquiry-with-rssi_example.py
@@ -78,6 +78,7 @@ def device_inquiry_with_with_rssi(sock):
     # before the inquiry is performed, bluez should flush its cache of
     # previously discovered devices
     flt = bluez.hci_filter_new()
+    print("flt: " + str(flt))
     bluez.hci_filter_all_events(flt)
     bluez.hci_filter_set_ptype(flt, bluez.HCI_EVENT_PKT)
     sock.setsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, flt )
@@ -85,8 +86,7 @@ def device_inquiry_with_with_rssi(sock):
     duration = 4
     max_responses = 255
     cmd_pkt = struct.pack("BBBBB", 0x33, 0x8b, 0x9e, duration, max_responses)
-    #bluez.hci_send_cmd(sock, bluez.OGF_LINK_CTL, bluez.OCF_INQUIRY, cmd_pkt)
-    bluez.hci_send_cmd(sock, bluez.OGF_LINK_CTL, "44:00:10:3F:2A:B8", cmd_pkt)
+    bluez.hci_send_cmd(sock, bluez.OGF_LINK_CTL, bluez.OCF_INQUIRY, cmd_pkt)
 
     results = []
 
