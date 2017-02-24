54c2779463a227318ac8b33a8e6be24388768425
chasemc67 <chasemc67@gmail.com>
2017-02-23 19:14:44 -0700

    Added some comments

--- main.py
+++ main.py
@@ -7,10 +7,11 @@
 from wifi import startWifiLoop
 
 def main():
+	# make sure these are lower case
 	targetWifiMacs = ["44:00:10:3f:2a:b7"]
-	targetBTMacs = ["44:00:10:3F:2A:B8"]
+	targetBTMacs = ["44:00:10:3f:2a:b8"]
 	targetWifiDistance = -40
-	targetBTDistance
+	targetBTDistance = -40
 
 
 	startWifiLoop("mon0", targetWifiMacs, targetWifiDistance)
