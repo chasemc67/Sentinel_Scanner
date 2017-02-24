4e9eb54cd9d5c47b99aea9763d73f0569ecd04b8
chasemc67 <chasemc67@gmail.com>
2017-02-23 19:17:33 -0700

    REmoved some imports that don't look needed

--- wifi.py
+++ wifi.py
@@ -1,39 +1,27 @@
-'''
-Packet sniffer in python using the pcapy python library
+# A module used to sniff for wifi macs within some range.
+# See scapy_ex.py for how the packets are parsed
  
-Project website
-http://oss.coresecurity.com/projects/pcapy.html
-'''
-
-# See this scapy rssi example
-# https://github.com/azz2k/scapy-rssi/blob/master/scapy-rssi.py
- 
-import socket
-from struct import *
-import datetime	
-import pcapy
-import sys
+#import socket
+#from struct import *
+#import datetime	
+#import pcapy
+#import sys
 import scapy.all as sca
 import scapy_ex
-import struct   
+#import struct   
 
 
-import time
-import thread
-import threading
-import signal
-import sys
+#import time
+#import thread
+#import threading
+#import signal
+#import sys
  
 def startWifiLoop(interface, targetList, distance):
     
     while True:
         packets = sca.sniff(iface=interface, count = 1)
         for pkt in packets:
-            #pkt.show()
-            # addr2 appears to be the source mac
-            #print("Mac: ") + str(pkt.addr2)
-            #print("Signal Strength: " + str(pkt.dBm_AntSignal))
-
             if pkt.addr2 in targetList:
                 if abs(pkt.dBm_AntSignal) <= abs(distance):
                     print("[+] " + str(pkt.addr2) + "Seen within range")
