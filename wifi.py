370aa24a8e474222f9e87ccab474bd8fb4ea13f0
chasemc67 <chasemc67@gmail.com>
2017-02-25 16:09:40 -0700

    Added some debug strings

--- wifi.py
+++ wifi.py
@@ -19,13 +19,15 @@ class WifiThread(threading.Thread):
 	def startWifiLoop(self):
 	    packets = sca.sniff(iface=self.interface, count = 1)
 	    for pkt in packets:
-	        if pkt.addr2 in self.targetList:
+	        if pkt.addr2.lower() in self.targetList:
 	            if abs(pkt.dBm_AntSignal) <= abs(self.distance):
 	                #print("[+] Wifi " + str(pkt.addr2) + " Seen within range")
 	                self.buzzer.put((True, pkt.addr2))
 	            else:
 	                #print("[-] Wifi " + str(pkt.addr2) + " Seen but not within range")
 	                self.buzzer.put((False, pkt.addr2))
+	        else:
+	        	self.buzzer.put((False, pkt.addr2))
 
 
 	def run(self):
